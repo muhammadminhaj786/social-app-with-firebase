b1b33050820e71e815bf6237149a75a88769c510
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-08-20 10:59:03 -0700

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,33 +1,55 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
-
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+import {  getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-auth.js";
+import {  getFirestore, doc, setDoc ,addDoc,collection,deleteDoc,getDocs,updateDoc,getDoc ,serverTimestamp} from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
+import {  getStorage ,deleteObject, ref, uploadBytes, uploadString, uploadBytesResumable, getDownloadURL} from "https://www.gstatic.com/firebasejs/10.2.0/firebase-storage.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-T-PF237_CGpuT0jcHvSG36sGB9ckAz8",
-  authDomain: "careernexa.firebaseapp.com",
-  projectId: "careernexa",
-  storageBucket: "careernexa.appspot.com",
-  messagingSenderId: "368656411778",
-  appId: "1:368656411778:web:34d49aee7431d1bdc5989b"
-};
+    apiKey: "AIzaSyC-T-PF237_CGpuT0jcHvSG36sGB9ckAz8",
+    authDomain: "careernexa.firebaseapp.com",
+    projectId: "careernexa",
+    storageBucket: "careernexa.appspot.com",
+    messagingSenderId: "368656411778",
+    appId: "1:368656411778:web:34d49aee7431d1bdc5989b"
+  };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize AUTH
 const auth = getAuth();
-
-// Initialize Cloud Firestore 
 const db = getFirestore(app);
 
+const storage = getStorage();
 
-export {
-    db,
+export{
+    app,
+    getAuth, 
+    createUserWithEmailAndPassword,
     auth,
-
+    getFirestore ,
+    db,
+    doc, 
+    setDoc,
+    signInWithEmailAndPassword,
+    addDoc,
+    collection,
+    deleteDoc,
+    getDocs,
+    signOut,
+    updateDoc,
+    getDoc,
+    getStorage,
+    storage,
+    deleteObject,
+    ref,
+    uploadBytes,
+    uploadString,
+    uploadBytesResumable,
+    getDownloadURL,
+    serverTimestamp,
+    
 }
